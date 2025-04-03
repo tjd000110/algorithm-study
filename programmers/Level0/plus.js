969b4eb9e8cf459ccdd29a535d449a0f0b0149f9
misung choi <tjd000110@gmail.com>
2025-04-04 01:07:51 +0900

    두수의 합

---
+++ programmers/Level0/plus.js
@@ -0,0 +1,24 @@
+//정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function solution(num1, num2) {
+    var answer = num1+num2;
+    return answer;
+}
+
+r1.question("첫번째 : ", (input1) => {
+    r1.question("두번째 : ", (input2) => {
+        const num1 = parseInt(input1);
+        const num2 = parseInt(input2);
+
+        console.log("합: ", solution(num1,num2))
+        r1.close();
+    })
+})
